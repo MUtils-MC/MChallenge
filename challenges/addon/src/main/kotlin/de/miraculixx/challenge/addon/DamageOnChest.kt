2c14d09ca0a30b19a9f2e7152f10fb5d7c0e52c4
Miraculixx <miraculixxyt@gmail.com>
2023-04-30 23:04:07 +0200

    Create template addon
    A template for challenge addons. Addons can inject their own challenges into MUtils and inject into the challenge system

--- challenges/addon/src/main/kotlin/de/miraculixx/challenge/addon/DamageOnChest.kt
+++ challenges/addon/src/main/kotlin/de/miraculixx/challenge/addon/DamageOnChest.kt
@@ -1,29 +1,32 @@
 package de.miraculixx.challenge.addon
 
 import de.miraculixx.challenge.api.modules.challenges.Challenge
-import de.miraculixx.challenge.api.settings.challenges
 import de.miraculixx.kpaper.event.listen
+import de.miraculixx.kpaper.event.register
+import de.miraculixx.kpaper.event.unregister
 import org.bukkit.Material
 import org.bukkit.event.player.PlayerInteractEvent
+import java.util.UUID
 
-class DamageOnChest : Challenge {
+class DamageOnChest(uuid: UUID) : Challenge {
     private val damage: Int
 
     init {
-
+        val settings = AddonManager.getSettings(uuid).settings
+        damage = settings["damage"]?.toInt()?.getValue() ?: 2
     }
 
     override fun register() {
-
+        onChestClick.register()
     }
 
     override fun unregister() {
-
+        onChestClick.unregister()
     }
 
-    private val onChestClick = listen<PlayerInteractEvent> {
+    private val onChestClick = listen<PlayerInteractEvent>(register = false) {
         val block = it.clickedBlock ?: return@listen
         if (block.type != Material.CHEST) return@listen
-        it.player.damage()
+        it.player.damage(damage.toDouble())
     }
 }
