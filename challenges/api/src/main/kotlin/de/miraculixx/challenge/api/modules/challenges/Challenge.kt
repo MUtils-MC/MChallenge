f3c4c5bb6179f2c02a8dde5d082a540d6dc5cba1
Miraculixx <miraculixxyt@gmail.com>
2023-05-11 19:07:56 +0200

    Add documentation

--- challenges/api/src/main/kotlin/de/miraculixx/challenge/api/modules/challenges/Challenge.kt
+++ challenges/api/src/main/kotlin/de/miraculixx/challenge/api/modules/challenges/Challenge.kt
@@ -1,5 +1,13 @@
 package de.miraculixx.challenge.api.modules.challenges
 
+/**
+ * Represents all mods.
+ *
+ * - [start] - Only called once on "/challenge start"
+ * - [stop] - Only called once on "/challenge stop" or plugin shutdown
+ * - [register] - Called once on "/challenge start" and on every resume with "/challenge resume"
+ * - [unregister] - Called once on "/challenge stop" and on every pause with "/challenge pause"
+ */
 interface Challenge {
     /**
      * Any start logic that is run *before* the challenge is registered. Return false to abort start up
