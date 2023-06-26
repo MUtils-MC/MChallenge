3bbf51c626e894b07ad1bb6d7bf1b6b9a2fd4504
Miraculixx <miraculixxyt@gmail.com>
2023-06-26 18:24:46 +0200

    Update Challenge Data

--- challenges/paper/src/main/kotlin/de/miraculixx/mchallenge/commands/NoAccountCommand.kt
+++ challenges/paper/src/main/kotlin/de/miraculixx/mchallenge/commands/NoAccountCommand.kt
@@ -1,6 +1,5 @@
 package de.miraculixx.mchallenge.commands
 
-import de.miraculixx.mchallenge.MChallenge
 import de.miraculixx.mvanilla.messages.cmp
 import de.miraculixx.mvanilla.messages.plus
 import de.miraculixx.mvanilla.messages.prefix
@@ -8,14 +7,13 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabExecutor
 
-class NoAccountCommand: TabExecutor {
+class NoAccountCommand : TabExecutor {
     override fun onTabComplete(sender: CommandSender, command: Command, label: String, args: Array<out String>?): MutableList<String> {
         return mutableListOf()
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        if (MChallenge.bridgeAPI != null) sender.sendMessage(prefix + cmp("This command is only available with an connected MUtils account! Use /login to connect"))
-        else sender.sendMessage(prefix + cmp("This command is only available with an connected MUtils account! Install MBridge to connect one (/ch bridge-install)"))
+        sender.sendMessage(prefix + cmp("This command is only available with an connected MUtils account! Use /login to connect"))
         return true
     }
 }
