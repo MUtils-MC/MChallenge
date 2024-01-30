e25e1dadffd66894b240e707c88775624791028f
maxbossing <info@maxbossing.de>
2024-01-30 15:24:55 +0100

    Bump versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("plugin.serialization") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 repositories {
@@ -15,22 +15,22 @@ repositories {
 
 dependencies {
     fun pluginDep(id: String, version: String) = "${id}:${id}.gradle.plugin:${version}"
-    val kotlinVersion = "1.9.10"
+    val kotlinVersion = "1.9.22"
 
     compileOnly(kotlin("gradle-plugin", kotlinVersion))
     runtimeOnly(kotlin("gradle-plugin", kotlinVersion))
     compileOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", kotlinVersion))
     runtimeOnly(pluginDep("org.jetbrains.kotlin.plugin.serialization", kotlinVersion))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
     // Fabric implementation
-    implementation("net.fabricmc:fabric-loom:1.3-SNAPSHOT")
+    implementation("net.fabricmc:fabric-loom:1.4-SNAPSHOT")
     implementation(pluginDep("io.github.juuxel.loom-quiltflower", "1.9.0"))
 
     // Paper implementation
-    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.5.8")
-    implementation(pluginDep("xyz.jpenilla.run-paper", "2.2.0"))
+    implementation("io.papermc.paperweight.userdev:io.papermc.paperweight.userdev.gradle.plugin:1.5.11")
+    implementation(pluginDep("xyz.jpenilla.run-paper", "2.2.2"))
 
-    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+    implementation("com.github.johnrengelman:shadow:8.1.1")
     implementation(pluginDep("com.modrinth.minotaur", "2.+"))
 }
--- buildSrc/src/main/kotlin/adventure-script.gradle.kts
+++ buildSrc/src/main/kotlin/adventure-script.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm")
 }
 
-val adventureVersion = "4.13.1"
+val adventureVersion = "4.15.0"
 dependencies {
     implementation("net.kyori:adventure-api:$adventureVersion")
     implementation("net.kyori:adventure-text-minimessage:$adventureVersion")
