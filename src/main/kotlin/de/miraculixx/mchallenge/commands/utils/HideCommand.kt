5d045fb6348c89030e919c6bd14b325fc82264b1
Miraculixx <miraculixxyt@gmail.com>
2025-01-22 17:04:12 +0100

    Improve commands usage

--- src/main/kotlin/de/miraculixx/mchallenge/commands/utils/HideCommand.kt
+++ src/main/kotlin/de/miraculixx/mchallenge/commands/utils/HideCommand.kt
@@ -2,33 +2,42 @@
 
 package de.miraculixx.mchallenge.commands.utils
 
+import de.miraculixx.kpaper.event.listen
 import de.miraculixx.kpaper.extensions.bukkit.msg
 import de.miraculixx.kpaper.extensions.onlinePlayers
 import de.miraculixx.mchallenge.PluginManager
-import de.miraculixx.mchallenge.utils.command
 import de.miraculixx.mcommons.text.plus
 import de.miraculixx.mcommons.text.prefix
 import dev.jorel.commandapi.CommandTree
 import dev.jorel.commandapi.kotlindsl.anyExecutor
+import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.jorel.commandapi.kotlindsl.entitySelectorArgumentManyPlayers
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerJoinEvent
 
 class HideCommand {
+    private val hiddenUsers = mutableSetOf<Player>()
+    private var initialized = false
+
     @Suppress("unused")
-    val hide = command("hide") {
+    val hide = commandTree("hide") {
         withPermission("mutils.hide")
         handleVisibility(true)
     }
 
     @Suppress("unused")
-    val show = command("show") {
+    val show = commandTree("show") {
         withPermission("mutils.hide")
         handleVisibility(false)
     }
 
     private fun CommandTree.handleVisibility(hide: Boolean) {
         playerExecutor { player, _ ->
+            if (hide) hiddenUsers.add(player)
+            else hiddenUsers.remove(player)
+
+            initializing()
             onlinePlayers.forEach { target ->
                 if (target == player) return@forEach
                 if (hide) target.hidePlayer(PluginManager, player)
@@ -39,6 +48,10 @@ class HideCommand {
         entitySelectorArgumentManyPlayers("player") {
             anyExecutor { sender, args ->
                 val sources = args[0] as Collection<Player>
+                if (hide) hiddenUsers.addAll(sources)
+                else hiddenUsers.removeAll(sources)
+
+                initializing()
                 onlinePlayers.forEach { target ->
                     sources.forEach { source ->
                         if (source != target) {
@@ -48,6 +61,20 @@ class HideCommand {
                     }
                 }
                 sender.sendMessage(prefix + sender.msg("command.hide.${if (hide) "hide" else "show"}Multiple", listOf(sources.size.toString())))
+            }
+        }
+    }
+
+    private fun initializing() {
+        if (initialized) return
+        initialized = true
+        listen<PlayerJoinEvent> {
+            hiddenUsers.forEach { it.hidePlayer(PluginManager, it) }
+            if (it.player in hiddenUsers) {
+                onlinePlayers.forEach { target ->
+                    if (target == it.player) return@forEach
+                    target.hidePlayer(PluginManager, it.player)
+                }
             }
         }
     }
--- src/main/kotlin/de/miraculixx/mchallenge/commands/utils/InvSeeCommand.kt
+++ src/main/kotlin/de/miraculixx/mchallenge/commands/utils/InvSeeCommand.kt
@@ -1,6 +1,7 @@
 package de.miraculixx.mchallenge.commands.utils
 
 import de.miraculixx.mchallenge.utils.command
+import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.jorel.commandapi.kotlindsl.playerArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 import org.bukkit.Sound
@@ -8,7 +9,7 @@ import org.bukkit.entity.Player
 
 class InvSeeCommand {
     @Suppress("unused")
-    val reset = command("invsee") {
+    val reset = commandTree("invsee") {
         withPermission("mutils.invsee")
         playerArgument("player") {
             playerExecutor { player, args ->
